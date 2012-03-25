53d3ade7185d638645efb9d924683a095aac1d15
Takashi Hayasaka <iwhurtafly@gmail.com>
2012-03-25 11:25:09 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Respira::Application.config.secret_token = 'c520cf65089a578cf5324c37cba251e7d34150e8f595a1abbba265d433c1c6ea461159aef497e0e9e5b9522c0c6db2839656c75f78999a3812f2957365303843'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Respira::Application.config.session_store :cookie_store, key: '_respira_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Respira::Application.config.session_store :active_record_store
