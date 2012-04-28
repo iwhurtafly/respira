05842b71b6996b60c5782805d017b56a9bc39049
Takashi Hayasaka <iwhurtafly@gmail.com>
2012-04-28 15:46:59 +0900

    mend

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Respira::Application.routes.draw do
   get "respira/home"
-
   get "respira/about"
-
   get "respira/webapp"
-
   get "respira/contact"
+
+  get "respira.heroku.com/home"
+  get "respira.heroku.com/about"
+  get "respira.heroku.com/webapp"
+  get "respira.heroku.com/contact"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
